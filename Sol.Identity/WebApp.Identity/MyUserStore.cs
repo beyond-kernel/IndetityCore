cfcd0b2772e742ae16a3eaf7bbef6a12d93442ad
Rafael Nogueira <rafanog.linux@hotmail.com>
2020-11-20 12:30:55 -0300

    first commit

---
+++ Sol.Identity/WebApp.Identity/MyUserStore.cs
@@ -0,0 +1,154 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace WebApp.Identity
+{
+    public class MyUserStore : IUserStore<MyUser>, IUserPasswordStore<MyUser>
+    {
+        public static DbConnection GetDbConnection()
+        {
+            var connection = new SqlConnection("Data Source=localhost;Initial Catalog=IdentityCurso;User id=sa;Password=123456;Integrated Security=SSPI;");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        public async Task<IdentityResult> CreateAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbConnection())
+            {
+                await connection.ExecuteAsync(
+                   @"INSERT INTO [Users]([Id],[UserName],[NormalizedUserName],[PasswordHash])
+                    VALUES
+                           (@id
+                           , @userName
+                           , @normalizedUserName
+                           , @passwordHash)",
+                   new
+                   {
+                       id = user.Id,
+                       userName = user.UserName,
+                       normalizedUserName = user.NormalizedUserName,
+                       passwordHash = user.PasswordHash
+                   });
+            }
+
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbConnection())
+            {
+                await connection.ExecuteAsync(
+                   "DELETE FROM Users WHERE Id = @id",
+                   new
+                   {
+                       id = user.Id
+                   });
+            }
+
+            return IdentityResult.Success;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public async Task<MyUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<MyUser>(
+                    "SELECT * FROM Users WHERE Id = @id",
+                    new { id = userId });
+            }
+        }
+
+        public async Task<MyUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<MyUser>(
+                    "SELECT * FROM Users WHERE NormalizedUserName = @name",
+                    new { name = normalizedUserName });
+            }
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.NormalizedUserName);
+        }
+
+        public Task<string> GetPasswordHashAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<string> GetUserIdAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task<bool> HasPasswordAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+
+        public Task SetNormalizedUserNameAsync(MyUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.NormalizedUserName = normalizedName;
+            return Task.CompletedTask;
+        }
+
+        public Task SetPasswordHashAsync(MyUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
+        }
+
+        public Task SetUserNameAsync(MyUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.UserName = userName;
+            return Task.CompletedTask;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbConnection())
+            {
+                await connection.ExecuteAsync(
+                   @"UPDATE [Users]
+                       SET[Id] = @id
+                          ,[UserName] = @userName
+                          ,[NormalizedUserName] = @normalizedUserName
+                          ,[PasswordHash] = @passwordHash
+                     WHERE[Id] = @id",
+                   new
+                   {
+                       id = user.Id,
+                       userName = user.UserName,
+                       normalizedUserName = user.NormalizedUserName,
+                       passwordHash = user.PasswordHash
+                   });
+            }
+
+            return IdentityResult.Success;
+        }
+    }
+}
---
+++ Sol.Identity/WebApp.Identity/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApp.Identity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            //services.AddIdentityCore<MyUser>(options => { });
+            //services.AddScoped<IUserStore<MyUser>, MyUserStore>();
+
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<MyUserDbContext>(
+                opt => opt.UseSqlServer("Data Source=localhost;Initial Catalog=IdentityCurso;User id=sa;Password=123456;Integrated Security=SSPI;", sql => sql.MigrationsAssembly(migrationAssembly))
+                );
+
+
+            services.AddIdentityCore<MyUser>(options => { });
+            services.AddScoped<IUserStore<MyUser>, UserOnlyStore<MyUser, MyUserDbContext>>();
+
+
+            services.AddAuthentication("cookies").AddCookie("cookies", opt => opt.LoginPath = "/Home/Login");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
